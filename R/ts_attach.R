b5eb7052192b3e5be76b65a5459435ef17069ea5
Christoph Sax <christoph.sax@gmail.com>
2021-01-31 19:03:55 +0100

    # workaround to save series name if one series is provided

--- R/ts_attach.R
+++ R/ts_attach.R
@@ -9,6 +9,9 @@
 ts_attach <- function(x, envir = globalenv()) {
   stopifnot(ts_boxable(x))
   tl <- ts_tslist(x)
+
+  # workaround to save series name if one series is provided
+  if (length(tl) == 1) names(tl) <- unique(x$id)
   message("Attached: ", paste(names(tl), collapse = ", "))
   Map(
     function(...) assign(..., envir = envir),
